4708089e5afd6b23ead8d2cd6f24b965d9d75d51
Phil Davis <phil@jankaritech.com>
2020-01-26 11:19:51 +0545

    Update dependencies and code style tools

--- tests/ParseAtomTest.php
+++ tests/ParseAtomTest.php
@@ -2,7 +2,9 @@
 
 namespace Sabre\Xml\Atom;
 
-class ParseAtomTest extends \PHPUnit_Framework_TestCase {
+use PHPUnit\Framework\TestCase;
+
+class ParseAtomTest extends TestCase {
 
     function testSimple() {
 
